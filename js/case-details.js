449351a8f37bc3030f664aee9364ce16c3192b45
nicholas-zeus <shunpyaemin@gmail.com>
2025-09-26 22:19:14 +0700

    update case-details.js

--- js/case-details.js
+++ js/case-details.js
@@ -4,6 +4,10 @@ import { getCase, createCase, updateCase, finishCase, undoFinish, statusLabel } 
 import { computeAge } from "/js/utils.js";
 import { toDate } from "/js/utils.js";
 import { fab } from "/js/fab.js";
+document.addEventListener("caseLoaded", () => {
+  fab.init?.();
+  fab.setTab?.("details");
+});
 
 // NEW CASE flow (inside loadCase -> state.isNew branch)
 
